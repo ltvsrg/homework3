c9fe3dc97928803fa9b086b7401183326272300f
ltvsrg <sealtaev@yandex.ru>
2017-06-19 13:21:39 +0900

    Add files via upload

---
+++ vxcv.php
@@ -0,0 +1,60 @@
+<?php
+
+$d=[];
+$q=[];
+$z=[];
+$dd=[];
+
+$a=['Africa'=>['Panthera leo', 'Diceros bicornis', 'Acinonyx jubatus', 'Giraffa camelopardalis', 'Loxodonta'],
+    'Australia'=>['Macropus', 'Ornithorhynchus anatinus', 'Phascolarctos cinereus', 'Dromaius novaehollandiae', 'Vombatidae'],
+    'South America'=>['Eunectes murinus', 'Pygocentrus nattereri', 'Chrysocyon brachyurus', 'Lama glama', 'Cingulata']
+];
+
+foreach ($a as $n){
+    foreach ($n as $f){
+        if (strpos($f, ' ')==true){
+            $d[]=$f;
+        }
+    }
+}
+
+for($i=0;$i<count($d);$i++) {
+    $s = explode(" ", $d[$i]);
+    if(count($s)>2){
+        unset($s);
+    }
+    else {
+        $q[] = $s[0];
+        $z[] = $s[1];
+    }
+}
+
+$gt=shuffle($q);
+$gth=shuffle($z);
+
+
+for($i=0;$i<count($q);$i++) {
+    for ($i = 0; $i < count($z); $i++) {
+        $yy = $q[$i] . " " . $z[$i];
+        $dd[]= $yy;
+    }
+}
+
+foreach($a as $id=>$ss) {
+    echo '<h2>', $id, '</h2>';
+    foreach ($ss as $gty=>$gf) {
+        for ($i=0;$i<count($dd);$i++) {
+            if (strncmp($dd[$i], $gf, 4) == 0 ) {
+                if($gty==3){
+                    echo $dd[$i];
+                }
+                else {
+                    echo $dd[$i], ', ';
+                    break;
+                }
+
+            }
+        }
+    }
+}
+?>
